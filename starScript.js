27edecb76ab81a8fbc8ea3ba9cf4dea242b262c9
lucas <lucas-p52@hotmail.fr>
2020-03-25 16:04:47 +0100

    ajout des noms colorés

---
+++ starScript.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.red('Julien'));
+console.log(chalk.blue('Victor'));
+console.log(chalk.yellow('Brian'));
+console.log(chalk.green('Angelique'));
+
